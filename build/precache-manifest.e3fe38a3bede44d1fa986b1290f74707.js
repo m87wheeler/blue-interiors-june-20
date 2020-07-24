0942311684244b3ba509e09f6cb2051aaf807da7
Martin <m87.wheeler@gmail.com>
2020-07-24 23:14:11 +0300

    Update gitignore
    Removed ignore /build for netlify

---
+++ build/precache-manifest.e3fe38a3bede44d1fa986b1290f74707.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3b61b7faccb7d7c9e5a2139971d1da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "254fe6e59026dd863372",
+    "url": "/static/js/2.4921ea98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4921ea98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec6523950c1bb40b0b7",
+    "url": "/static/js/main.1e602aee.chunk.js"
+  },
+  {
+    "revision": "181a39e9f7a54e2396c2",
+    "url": "/static/js/runtime-main.ae34fef4.js"
+  },
+  {
+    "revision": "38c8f2a4ba27f2b3977bd7065d7af40e",
+    "url": "/static/media/hero-mobile.38c8f2a4.png"
+  },
+  {
+    "revision": "eb6173bbc950b11feefb8bd4a673cf61",
+    "url": "/static/media/logo384.eb6173bb.png"
+  },
+  {
+    "revision": "cb4699cfdea6dd806b326c1b2ea09946",
+    "url": "/static/media/portrait-image.cb4699cf.jpg"
+  },
+  {
+    "revision": "fa7678c16cc60fb920100cf8e4935a12",
+    "url": "/static/media/service-image-1.fa7678c1.jpg"
+  },
+  {
+    "revision": "51808a58125551c9b38a8f362efc32f6",
+    "url": "/static/media/service-image-2.51808a58.jpg"
+  },
+  {
+    "revision": "d5a83b20f0e31580fdb32a14e8b02a5b",
+    "url": "/static/media/service-image-3.d5a83b20.jpg"
+  }
+]);
