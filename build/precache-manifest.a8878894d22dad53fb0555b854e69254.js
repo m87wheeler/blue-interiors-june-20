34ebbda3ff48c48992f3ccb97ef3852fc01f8b44
Martin <m87.wheeler@gmail.com>
2020-08-03 16:52:40 +0300

    Update to files prior to hosting
    Hosting to Netlify

---
+++ build/precache-manifest.a8878894d22dad53fb0555b854e69254.js
@@ -0,0 +1,55 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+<<<<<<< HEAD:build/precache-manifest.a8878894d22dad53fb0555b854e69254.js
+    "revision": "f53d8315e063cd37b3fff45d7a9acaeb",
+=======
+    "revision": "844832e3b5eeb34bb3357933f8bfc98f",
+>>>>>>> aeae529960e2b08751194a4de265b4ce27fd731f:build/precache-manifest.fd0f8f83926e681e474eba9f4298b607.js
+    "url": "/index.html"
+  },
+  {
+    "revision": "88f0e9e6026bc49b2f9f",
+    "url": "/static/js/2.707e5e1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.707e5e1d.chunk.js.LICENSE.txt"
+  },
+  {
+<<<<<<< HEAD:build/precache-manifest.a8878894d22dad53fb0555b854e69254.js
+    "revision": "733a8bb4a7f1b38bafa2",
+    "url": "/static/js/main.973e74db.chunk.js"
+=======
+    "revision": "6214ccef58ea7b809d75",
+    "url": "/static/js/main.d1d5a8a0.chunk.js"
+>>>>>>> aeae529960e2b08751194a4de265b4ce27fd731f:build/precache-manifest.fd0f8f83926e681e474eba9f4298b607.js
+  },
+  {
+    "revision": "181a39e9f7a54e2396c2",
+    "url": "/static/js/runtime-main.ae34fef4.js"
+  },
+  {
+    "revision": "38c8f2a4ba27f2b3977bd7065d7af40e",
+    "url": "/static/media/hero-mobile.38c8f2a4.png"
+  },
+  {
+    "revision": "a3392b1536aae1f0131a4590f2d3053c",
+    "url": "/static/media/logo384.a3392b15.png"
+  },
+  {
+    "revision": "cb4699cfdea6dd806b326c1b2ea09946",
+    "url": "/static/media/portrait-image.cb4699cf.jpg"
+  },
+  {
+    "revision": "fa7678c16cc60fb920100cf8e4935a12",
+    "url": "/static/media/service-image-1.fa7678c1.jpg"
+  },
+  {
+    "revision": "51808a58125551c9b38a8f362efc32f6",
+    "url": "/static/media/service-image-2.51808a58.jpg"
+  },
+  {
+    "revision": "d5a83b20f0e31580fdb32a14e8b02a5b",
+    "url": "/static/media/service-image-3.d5a83b20.jpg"
+  }
+]);
